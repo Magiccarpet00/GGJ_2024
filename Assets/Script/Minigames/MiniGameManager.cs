125f15db272f5b1eead23e35d0e387bd6057aca8
BaldrikLeKaiser <petimousse.geniedumal@gmail.com>
2024-01-27 14:10:18 +0100

    Update scene manager et difficulty

--- Assets/Script/Minigames/MiniGameManager.cs
+++ Assets/Script/Minigames/MiniGameManager.cs
@@ -5,22 +5,43 @@ using UnityEngine;
 
 public class MiniGameManager : MonoBehaviour
 {
+    [SerializeField] private LoadingScreen close;
+    [SerializeField] private LoadingScreen open;
     public event Action OnLose;
     public event Action OnWin;
+	public int difficultyParameter;
 
-    public void Win()
+	private void Awake()
 	{
-        OnWin?.Invoke();
-    }
 
-    public void Lose()
-	{
-        OnLose?.Invoke();
 	}
 
-    public IEnumerator Test()
+	public void Win()
+	{
+		open.gameObject.SetActive(true);
+		open.StartLoading += Open_Win;
+    }
+
+	private void Open_Win()
+	{
+		
+		OnWin?.Invoke();
+	}
+
+	public virtual void Lose()
+	{
+		open.gameObject.SetActive(true);
+		open.StartLoading += Open_Lose;
+	}
+
+	private void Open_Lose()
+	{
+		OnLose?.Invoke();
+	}
+
+	public IEnumerator Test()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(5);
 
         Win();
     }
