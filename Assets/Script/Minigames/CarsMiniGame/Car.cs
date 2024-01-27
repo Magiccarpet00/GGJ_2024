83f7103513e01a95e89e4bbe09ade1a3c66ad1ef
BaldrikLeKaiser <petimousse.geniedumal@gmail.com>
2024-01-27 16:04:16 +0100

    Add sound folder

---
+++ Assets/Script/Minigames/CarsMiniGame/Car.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private Animator right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
